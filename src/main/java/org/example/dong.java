d313406e8495180e8d88e4e7f1f492d7f3211347
doan van dong <nhokhongquen1981999@gmail.com>
2023-08-24 20:35:00 +0700

    chinh sua nhanh 3333333333

--- src/main/java/org/example/dong.java
+++ src/main/java/org/example/dong.java
@@ -4,5 +4,6 @@ public class dong {
     public static void main(String[] args) {
         System.out.println("doan van dong");
         System.out.println("nhanh 3");
+        System.out.println("chinh sua nhanh 3");
     }
 }
