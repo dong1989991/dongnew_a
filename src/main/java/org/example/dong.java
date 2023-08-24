cf19bb4594d3f422c5aea430a9538cc20f2f2e9e
doan van dong <nhokhongquen1981999@gmail.com>
2023-08-24 20:18:48 +0700

    tao nhanh 3

--- src/main/java/org/example/dong.java
+++ src/main/java/org/example/dong.java
@@ -3,5 +3,6 @@ package org.example;
 public class dong {
     public static void main(String[] args) {
         System.out.println("doan van dong");
+        System.out.println("nhanh 3");
     }
 }
