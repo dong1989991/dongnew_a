1f5a24e5ffe17386190924cb71914f6422a30496
thanh <thanhkaka9119@gmail.com>
2023-08-26 23:57:02 +0700

    nhanh 7

--- src/main/java/org/example/nhanh_77.java
+++ src/main/java/org/example/nhanh_77.java
@@ -1,7 +1,7 @@
 package org.example;
 
-public class nhanh_66 {
+public class nhanh_77 {
     public static void main(String[] args) {
-        System.out.println("nhanh 66");
+        System.out.println("nhanh 77");
     }
 }
