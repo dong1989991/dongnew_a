ae1acf4aff7d1840782c8783b2b5cd88c75b2091
doan van dong <nhokhongquen1981999@gmail.com>
2023-08-27 11:43:57 +0700

    dong nhanh 5

--- src/main/java/org/example/nhanh_55.java
+++ src/main/java/org/example/nhanh_55.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class nhanh_55 {
     public static void main(String[] args) {
-        System.out.println("nhanh 5");
+        System.out.println("nhanh 5eeeeeegit ");
     }
 }
