0057395f32ebdd923f2a213b834ad89d1d318c9d
doan van dong <nhokhongquen1981999@gmail.com>
2023-08-27 11:34:39 +0700

    lan 3

--- src/main/java/org/example/nhanh_66.java
+++ src/main/java/org/example/nhanh_66.java
@@ -4,5 +4,6 @@ public class nhanh_66 {
     public static void main(String[] args) {
         System.out.println("nhanh 666666666666");
         System.out.println("nhanh 6 lasn 2");
+        System.out.println("nhanh 6 lan 3git ");
     }
 }
