fa614b4d92cd3c8bc17f564ba774bef32f2a2501
doan van dong <nhokhongquen1981999@gmail.com>
2023-08-27 10:20:26 +0700

    dong lan 2

--- src/main/java/org/example/dong2.java
+++ src/main/java/org/example/dong2.java
@@ -3,5 +3,6 @@ package org.example;
 public class dong2 {
     public static void main(String[] args) {
         System.out.println("may dong");
+        System.out.println("may dong lan 2");
     }
 }
