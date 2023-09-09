99566d2b1c5755a1495ad93d6358cc703241235a
VanNguyen0113 <vanvit13011999@gmail.com>
2023-09-09 14:36:30 +0700

    ddd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("doan van dong");
+        System.out.println("van dang here");
     }
 }
