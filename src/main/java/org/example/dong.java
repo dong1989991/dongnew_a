f1b553a3e8b85d8dd447a742529b89a6a16f2611
doan van dong <nhokhongquen1981999@gmail.com>
2023-09-09 12:51:03 +0700

    change

--- src/main/java/org/example/dong.java
+++ src/main/java/org/example/dong.java
@@ -3,5 +3,7 @@ package org.example;
 public class dong {
     public static void main(String[] args) {
         System.out.println("thanh ve");
+        System.out.println("nhanh van ne");
+        System.out.println("hello");
     }
 }
